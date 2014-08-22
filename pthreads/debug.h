fc431c3eed823cf75e40cae5527e197a8f7a0d8f
Szilárd Biró <col.lawrence@gmail.com>
2014-08-22 21:47:40 +0200

    Added a lightweight pthreads subset

---
+++ pthreads/debug.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2002 Hyperion Entertainment
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef __DLL_DEBUG_H
+#define __DLL_DEBUG_H
+
+#ifdef __AROS__
+	#include <aros/debug.h>
+#else
+	#ifdef DEBUG
+		#define D(x) x
+	#else
+		#define D(x)
+	#endif
+	#ifdef __amigaos4__
+		#include <proto/exec.h>
+		#define bug DebugPrintF
+	#else
+		#include <clib/debug_protos.h>
+		#define bug kprintf
+	#endif
+#endif
+
+#endif
---
+++ pthreads/pthread.c
@@ -0,0 +1,457 @@
+/*
+  Copyright (C) 2014 Szilard Biro
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
+#include <clib/alib_protos.h>
+#include <proto/exec.h>
+#include <proto/dos.h>
+
+#include <errno.h>
+#include <setjmp.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "pthread.h"
+#include "debug.h"
+
+#define FALLBACKSIGNAL SIGBREAKB_CTRL_E
+
+struct cond_waiter
+{
+	struct MinNode node;
+	struct Task *task;
+	ULONG sigmask;
+};
+
+typedef struct
+{
+	void *(*start)(void *);
+	void *arg;
+	struct MsgPort *msgport;
+	struct Message msg;
+	struct Process *process;
+	//pthread_t id;
+	void *ret;
+	jmp_buf jmp;
+	const pthread_attr_t *attr;
+	char name[256];
+} ThreadInfo;
+
+// TODO: make this a list
+static ThreadInfo threads[PTHREAD_THREADS_MAX];
+static pthread_t nextid = 0;
+
+// helper functions
+static int SemaphoreIsInvalid(struct SignalSemaphore *sem)
+{
+	return (!sem || sem->ss_Link.ln_Type != NT_SIGNALSEM || sem->ss_WaitQueue.mlh_Tail != NULL);
+}
+
+static ThreadInfo *GetThreadInfo(pthread_t thread)
+{
+	ThreadInfo *inf;
+	inf = &threads[thread];
+	return inf;
+}
+
+//
+// Mutex functions
+//
+
+int pthread_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr)
+{
+	D(bug("%s(%p, %p)\n", __FUNCTION__, mutex, attr));
+
+	if (mutex == NULL)
+		return EINVAL;
+
+	InitSemaphore(&mutex->semaphore);
+
+	return 0;
+}
+
+int pthread_mutex_destroy(pthread_mutex_t *mutex)
+{
+	D(bug("%s(%p)\n", __FUNCTION__, mutex));
+
+	if (mutex == NULL)
+		return EINVAL;
+
+	memset(mutex, 0, sizeof(pthread_mutex_t));
+
+	return 0;
+}
+
+int pthread_mutex_lock(pthread_mutex_t *mutex)
+{
+	D(bug("%s(%p)\n", __FUNCTION__, mutex));
+
+	if (mutex == NULL)
+		return EINVAL;
+
+	// initialize static mutexes
+	if (SemaphoreIsInvalid(&mutex->semaphore))
+		pthread_mutex_init(mutex, NULL);
+
+	// TODO: non-recursive mutexes?
+	ObtainSemaphore(&mutex->semaphore);
+
+	return 0;
+}
+
+int pthread_mutex_unlock(pthread_mutex_t *mutex)
+{
+	D(bug("%s(%p)\n", __FUNCTION__, mutex));
+
+	if (mutex == NULL)
+		return EINVAL;
+
+	// initialize static mutexes
+	if (SemaphoreIsInvalid(&mutex->semaphore))
+		pthread_mutex_init(mutex, NULL);
+
+	ReleaseSemaphore(&mutex->semaphore);
+
+	return 0;
+}
+
+//
+// Condition variable functions
+//
+
+int pthread_cond_init(pthread_cond_t *cond, const pthread_condattr_t *attr)
+{
+	D(bug("%s(%p, %p)\n", __FUNCTION__, cond, attr));
+
+	if (cond == NULL)
+		return EINVAL;
+
+	InitSemaphore(&cond->semaphore);
+	NewList((struct List *)&cond->waiters);
+	cond->waiting = 0;
+
+	return 0;
+}
+
+int pthread_cond_destroy(pthread_cond_t *cond)
+{
+	D(bug("%s(%p)\n", __FUNCTION__, cond));
+
+	if (cond == NULL)
+		return EINVAL;
+
+	if (cond->waiting > 0)
+		return EBUSY;
+
+	memset(cond, 0, sizeof(pthread_cond_t));
+
+	return 0;
+}
+
+int pthread_cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex)
+{
+	struct cond_waiter waiter;
+	BYTE signal;
+
+	D(bug("%s(%p, %p)\n", __FUNCTION__, cond, mutex));
+
+	if (cond == NULL || mutex == NULL)
+		return EINVAL;
+
+	// initialize static conditions
+	if (SemaphoreIsInvalid(&cond->semaphore))
+		pthread_cond_init(cond, NULL);
+
+	waiter.task = FindTask(NULL);
+	signal = AllocSignal(-1);
+	if (signal == -1)
+	{
+		signal = FALLBACKSIGNAL;
+		SetSignal(1 << FALLBACKSIGNAL, 0);
+	}
+	waiter.sigmask = 1 << signal;
+	ObtainSemaphore(&cond->semaphore);
+	AddTail((struct List *)&cond->waiters, (struct Node *)&waiter);
+	cond->waiting++;
+	ReleaseSemaphore(&cond->semaphore);
+
+	pthread_mutex_unlock(mutex);
+	Wait(waiter.sigmask);
+	pthread_mutex_lock(mutex);
+
+	ObtainSemaphore(&cond->semaphore);
+	Remove((struct Node *)&waiter);
+	cond->waiting--;
+	ReleaseSemaphore(&cond->semaphore);
+
+	if (signal != FALLBACKSIGNAL)
+		FreeSignal(signal);
+
+	return 0;
+}
+
+int pthread_cond_signal(pthread_cond_t *cond)
+{
+	struct cond_waiter *waiter;
+
+	D(bug("%s(%p)\n", __FUNCTION__, cond));
+
+	if (cond == NULL)
+		return EINVAL;
+
+	// initialize static conditions
+	if (SemaphoreIsInvalid(&cond->semaphore))
+		pthread_cond_init(cond, NULL);
+
+	ObtainSemaphore(&cond->semaphore);
+	if (cond->waiting > 0)
+	{
+		waiter = (struct cond_waiter *)cond->waiters.mlh_Head;
+		if (waiter->node.mln_Succ)
+			Signal(waiter->task, waiter->sigmask);
+	}
+	ReleaseSemaphore(&cond->semaphore);
+
+	return 0;
+}
+
+//
+// Thread attribute functions
+//
+
+int pthread_attr_init(pthread_attr_t *attr)
+{
+	D(bug("%s(%p)\n", __FUNCTION__, attr));
+
+	if (attr == NULL)
+		return EINVAL;
+
+	memset(attr, 0, sizeof(pthread_attr_t));
+	// TODO: sensible defaults?
+}
+
+int pthread_attr_destroy(pthread_attr_t *attr)
+{
+	D(bug("%s(%p)\n", __FUNCTION__, attr));
+
+	if (attr == NULL)
+		return EINVAL;
+
+	memset(attr, 0, sizeof(pthread_attr_t));
+}
+
+int pthread_attr_setschedparam(pthread_attr_t *attr, const struct sched_param *param)
+{
+	D(bug("%s(%p, %p)\n", __FUNCTION__, attr, param));
+
+	if (attr == NULL)
+		return EINVAL;
+
+	attr->param = *param;
+}
+
+//
+// Thread functions
+//
+
+/*
+   struct PPCStackSwapArgs swapargs;
+   swapargs.Args[0] = (ULONG)arg;
+   return NewPPCStackSwap(stack, function, &swapargs);
+
+   struct StackSwapArgs swapargs;
+   swapargs.Args[0] = (IPTR)arg;
+   return NewStackSwap(stack, function, &swapargs);
+*/
+
+static void StarterFunc(void)
+{
+	ThreadInfo *inf;
+
+	D(bug("%s()\n", __FUNCTION__));
+
+	inf = (ThreadInfo *)FindTask(NULL)->tc_UserData;
+
+	if (!setjmp(inf->jmp))
+		inf->ret = inf->start(inf->arg);
+
+	Forbid();
+	ReplyMsg(&inf->msg);
+}
+
+// TODO: we assume that sub-threads can only be started from the main one, which is NOT thread-safe
+// a semaphore could be added 
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start)(void *), void *arg)
+{
+	ThreadInfo *inf;
+
+	D(bug("%s(%p, %p, %p, %p)\n", __FUNCTION__, thread, attr, start, arg));
+
+	if (thread == NULL || start == NULL)
+		return EINVAL;
+
+	inf = GetThreadInfo(nextid);
+	inf->start = start;
+	inf->attr = attr;
+	inf->arg = arg;
+	snprintf(inf->name, sizeof(inf->name), "pthread thread #%d", nextid);
+
+	inf->msgport = CreateMsgPort();
+	if (!inf->msgport)
+	{
+		free(inf);
+		return EAGAIN;
+	}
+
+	inf->msg.mn_Node.ln_Type = NT_MESSAGE;
+	inf->msg.mn_ReplyPort = inf->msgport;
+	inf->msg.mn_Length = sizeof(inf->msg);
+
+	inf->process = CreateNewProcTags(NP_Entry, StarterFunc,
+#ifdef __MORPHOS__
+		NP_CodeType, CODETYPE_PPC,
+		(attr && attr->stacksize > 0) ? NP_PPCStackSize : TAG_IGNORE, attr->stacksize,
+#else
+		(attr && attr->stacksize > 0) ? NP_StackSize : TAG_IGNORE, attr->stacksize,
+#endif
+		NP_UserData, inf,
+		(attr) ? NP_Priority : TAG_IGNORE, attr->param.sched_priority,
+		NP_Name, inf->name,
+		TAG_DONE);
+
+	if (!inf->process)
+	{
+		DeleteMsgPort(inf->msgport);
+		free(inf);
+		return EAGAIN;
+	}
+
+	return nextid++;
+}
+
+int pthread_join(pthread_t thread, void **value_ptr)
+{
+	ThreadInfo *inf;
+
+	D(bug("%s(%u, %p)\n", __FUNCTION__, thread, value_ptr));
+
+	inf = GetThreadInfo(thread);
+
+	//while (!GetMsg(inf->msgport))
+		WaitPort(inf->msgport);
+
+	DeleteMsgPort(inf->msgport);
+	if (value_ptr)
+		*value_ptr = inf->ret;
+
+	return 0;
+}
+
+int pthread_equal(pthread_t t1, pthread_t t2)
+{
+	D(bug("%s(%u, %u)\n", __FUNCTION__, t1, t2));
+
+	return (t1 == t2);
+}
+
+pthread_t pthread_self(void)
+{
+	pthread_t i;
+	struct Task *task;
+
+	D(bug("%s()\n", __FUNCTION__));
+
+	task = FindTask(NULL);
+
+	for (i = 0; i < PTHREAD_THREADS_MAX; i++)
+	{
+		if ((struct Task *)threads[i].process == task)
+			break;
+	}
+
+	return i;
+}
+
+void pthread_exit(void *value_ptr)
+{
+	ThreadInfo *inf;
+
+	D(bug("%s(%p)\n", __FUNCTION__, value_ptr));
+
+	inf = GetThreadInfo(pthread_self());
+	inf->ret = value_ptr;
+
+	longjmp(inf->jmp, 1);
+}
+
+//
+// Scheduling functions
+//
+
+int pthread_setschedparam(pthread_t thread, int policy, const struct sched_param *param)
+{
+	ThreadInfo *inf;
+
+	D(bug("%s(%u, %d, %p)\n", __FUNCTION__, thread, policy, param));
+
+	if (param = NULL)
+		return EINVAL;
+
+	inf = GetThreadInfo(thread);
+	SetTaskPri((struct Task *)inf->process, param->sched_priority);
+
+	return 0;
+}
+
+//
+// NP
+//
+int pthread_setname_np(pthread_t thread, const char *name)
+{
+	ThreadInfo *inf;
+
+	D(bug("%s(%u, %s)\n", __FUNCTION__, thread, name));
+
+	inf = GetThreadInfo(thread);
+
+	if (strlen(name) + 1 > sizeof(inf->name))
+		return ERANGE;
+
+	strncpy(inf->name, name, sizeof(inf->name));
+
+	return 0;
+}
+
+int pthread_getname_np(pthread_t thread, char *name, size_t len)
+{
+	ThreadInfo *inf;
+
+	D(bug("%s(%u, %p, %u)\n", __FUNCTION__, thread, name, len));
+
+	inf = GetThreadInfo(thread);
+
+	if (strlen(inf->name) + 1 > len)
+		return ERANGE;
+	// TODO: partially copy the name?
+	strncpy(name, inf->name, len);
+
+	return 0;
+}
+
+
